f851a85cf09cab37efb07111b4b60ddd5a29891b
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-10-10 13:45:42 -0400

    Updating html form and backend operations

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, session
 import os
+from parser import *
 
 app = Flask(__name__)
 secret_key = str(os.urandom(24))
@@ -8,6 +9,12 @@ app.secret_key = secret_key
 @app.route("/")
 def hello():
     return render_template('mainpage.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    url = request.form['Reddit URL']
+    search_term = request.form['Keyword']
+    return parser.find_relevant_comments(url, search_term)
 
 if __name__ == "__main__":
     app.run()
--- parser.py
+++ parser.py
@@ -1,24 +1,33 @@
 """ Python Script for Displaying Comments Based On Keywords """
-
 import praw
-SEARCH_TERMS = ["Amazon", "amazon", "OA2"]
-CREDENTIALS = []
-with open(".gitignore") as f:
-    content = f.readlines()
-    for item in content:
-        item_len = len(item)
-        item = str(item[0:item_len-1])
-        CREDENTIALS.append(item)
-print(CREDENTIALS)
-reddit = praw.Reddit(user_agent='Comment Extraction via keywordit',
-                     client_id=CREDENTIALS[0], client_secret=CREDENTIALS[1])
 
-submission = reddit.submission(url='https://www.reddit.com/r/cscareerquestions/comments/9mx8r1/big_4_discussion_october_10_2018/')
+def find_relevant_comments(url, search_term):
+    CREDENTIALS = []
+    with open(".gitignore") as f:
+        content = f.readlines()
+        for item in content:
+            item_len = len(item)
+            item = str(item[0:item_len-1])
+            CREDENTIALS.append(item)
+    try:
+        reddit = praw.Reddit(user_agent='Comment Extraction via keywordit',
+                             client_id=CREDENTIALS[0], client_secret=CREDENTIALS[1])
+    except:
+        return "Invalid Session Information"
+    try:
+        submission = reddit.submission(url=str(url)):
+    except:
+        return "Invalid URL"
+    return return_comment_list(submission, keyword)
 
-for top_level_comment in submission.comments:
-    for keyword in SEARCH_TERMS:
-        if keyword in top_level_comment.body:
-            print(top_level_comment.body)
-            all_replies = top_level_comment.replies
-            for comment in all_replies:
-                print("^  " + comment.body)
+def return_comment_list(submission, keyword):
+    try:
+        for top_level_comment in submission.comments:
+            if keyword in top_level_comment.body:
+                print(top_level_comment.body)
+                all_replies = top_level_comment.replies
+                for comment in all_replies:
+                    print("^  " + comment.body)
+        return
+    except:
+        return "Error while parsing comments"
